19beb51be9f8a7270e357fb89554a26e17f42439
nate-nemeth27 <74068137+nate-nemeth27@users.noreply.github.com>
2023-04-23 21:59:04 -0400

    Update driver.js

--- driver.js
+++ driver.js
@@ -4,35 +4,41 @@ var qs = require('querystring');
 
 http.createServer(function (req, res) 
   {
-	  if (req.url == "/track")
-	  {
-		  file = 'track.html';
-		  fs.readFile(file, function(err, txt) {
-    	  res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(txt);
-          res.end();
-		  });
-        
-		 pdata = "";
-		 req.on('data', data => {
-           pdata += data.toString();
-         })
+	// console.log("Beginning");
+	// console.log(req.url);
+	// if (req.url == "/track")
+	//   {
+		// console.log("track");
 
+		file = 'track.html';
+		fs.readFile(file, 'utf8', function(err, txt) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(txt);
+			res.end();
+		});
+		console.log("after readfile");
+		// pdata = "";
+		// req.on('data', data => {
+		// 	pdata += data.toString();
+		// })
+		// console.log(pdata);
+		// console.log("after pdata");
 
 
 		// when complete POST data is received
-		req.on('end', () => {
-			pdata = qs.parse(pdata);
-			// res.write ("The name is: "+ pdata['the_name']);
-            res.write("OK");
-			// res.end();
-        });
-      }
-	  else {
-		  res.writeHead(200, {'Content-Type':'text/html'});
-		  res.write ("Unknown page request");
-		  res.end();
-	  }
+		// req.on('end', () => {
+		// 	pdata = qs.parse(pdata);
+		// 	res.write ("The name is: "+ pdata['the_name']);
+        //     res.write("OK");
+		// 	console.log("end of end");
+		// 	res.end();
+        // });
+    //   }
+	//   else {
+	// 	  res.writeHead(200, {'Content-Type':'text/html'});
+	// 	  res.write ("Unknown page request");
+	// 	  res.end();
+	//   }
   
 
 }).listen(3000);
